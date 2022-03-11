8d93ae64a9452ead9c3d20fa0753ba617c64367f
Natthanon Manop <natthanon.manop@gmail.com>
2022-03-11 11:46:00 +0700

    finish get with no go routine

--- main.go
+++ main.go
@@ -22,12 +22,185 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
+func select_create(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,author,message,likes FROM data_record WHERE created_at > ? AND deleted=0", tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Author, &rec.Message, &rec.Likes)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_author(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,author FROM data_record WHERE author_updated_at > ? AND (message_updated_at <= ? OR message_updated_at IS NULL) AND (likes_updated_at <= ? OR likes_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Author)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_message(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,message FROM data_record WHERE message_updated_at > ? AND (author_updated_at <= ? OR author_updated_at IS NULL) AND (likes_updated_at <= ? OR likes_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Message)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_likes(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,likes FROM data_record WHERE likes_updated_at > ? AND (author_updated_at <= ? OR author_updated_at IS NULL) AND (message_updated_at <= ? OR message_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Likes)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_author_message(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,author,message FROM data_record WHERE author_updated_at > ? AND message_updated_at > ? AND (likes_updated_at <= ? OR likes_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Author, &rec.Message)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_author_likes(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,author,likes FROM data_record WHERE author_updated_at > ? AND likes_updated_at > ? AND (message_updated_at <= ? OR message_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Author, &rec.Likes)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_message_likes(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,message,likes FROM data_record WHERE message_updated_at > ? AND likes_updated_at > ? AND (author_updated_at <= ? OR author_updated_at IS NULL) AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Message, &rec.Likes)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+func select_update_auther_message_likes(db *sql.DB, tm time.Time) []entities.ResponseData {
+	selDB, err := db.Query("SELECT uuid,author,message,likes FROM data_record WHERE author_updated_at > ? AND message_updated_at > ? AND likes_updated_at > ? AND updated=1 AND deleted=0", tm, tm, tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	records := []entities.ResponseData{}
+	for selDB.Next() {
+		var rec entities.ResponseData
+		err = selDB.Scan(&rec.UUID, &rec.Author, &rec.Message, &rec.Likes)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, rec)
+	}
+	return records
+}
+
+func select_delete(db *sql.DB, tm time.Time) []string {
+	selDB, err := db.Query("SELECT uuid FROM data_record WHERE created_at > ? AND deleted=1", tm)
+	if err != nil {
+		panic(err.Error())
+	}
+	var records []string
+	for selDB.Next() {
+		var uuid string
+		err = selDB.Scan(&uuid)
+		if err != nil {
+			panic(err.Error())
+		}
+		records = append(records, uuid)
+	}
+	return records
+}
+
 func get(c *gin.Context) {
 	var requestDatetime entities.GetRequestBody
 	c.BindJSON(&requestDatetime)
+	db := database.Connect()
+	defer db.Close()
 	fmt.Println(requestDatetime.UnixTimestamp)
 	tm := time.Unix(int64(requestDatetime.UnixTimestamp), 0).UTC()
 	fmt.Println(tm)
+	create_list := select_create(db, tm)
+	delete_list := select_delete(db, tm)
+	update := []entities.ResponseData{}
+	// go routine
+	update_author_list := select_update_author(db, tm)
+	update_message_list := select_update_message(db, tm)
+	update_likes_list := select_update_likes(db, tm)
+	update_author_message_list := select_update_author_message(db, tm)
+	update_author_likes_list := select_update_author_likes(db, tm)
+	update_message_likes_list := select_update_message_likes(db, tm)
+	update_auther_message_likes_list := select_update_auther_message_likes(db, tm)
+	// combine data
+	update = append(update, update_author_list...)
+	update = append(update, update_message_list...)
+	update = append(update, update_likes_list...)
+	update = append(update, update_author_message_list...)
+	update = append(update, update_author_likes_list...)
+	update = append(update, update_message_likes_list...)
+	update = append(update, update_auther_message_likes_list...)
+	m := map[string]interface{}{
+		"create": create_list,
+		"delete": delete_list,
+		"update": update,
+	}
+	c.JSON(200, m)
 }
 
 func post(c *gin.Context) {
